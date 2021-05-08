0968a5b156829d478ad7aeec2543674cf3e0924c
WearyWasTaken <67983460+WearyWasTaken@users.noreply.github.com>
2021-05-08 13:41:23 -0400

    Completed Methods 1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -3,7 +3,17 @@ import java.io.*;
 
 public class Utility {
 
-    
+    /**
+     * Replaces all zips, zaps, and zops with zp
+     * @author A. Wong
+     */
+    public static String zipZap(String methodInput){
+
+        methodInput = methodInput.replaceAll("zap","zp").replaceAll("zip","zp").replaceAll("zop","zp");
+
+        return methodInput;
+
+    }
 
     
 }
