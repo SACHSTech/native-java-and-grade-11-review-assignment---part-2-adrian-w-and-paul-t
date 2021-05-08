61114757f0a091cdf94f022fd8dd6006bbe09b5c
Paul Tran <>
2021-05-08 22:28:36 +0000

    methods 2 finished

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,11 @@
 package gr11review.part2;
 
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    System.out.println(Utility.sumNumbers("5233 87 hasdfas"));
+
+  }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,31 @@
 package gr11review.part2;
+
 import java.io.*;
 
 public class Utility {
 
-    
+  public static int sumNumbers(String str) {
 
-    
+    char letter;
+    int strLength = str.length();
+    int returnSum = 0;
+    int intermediate = 0;
+
+    for (int i = 0; i < strLength; i++) {
+      letter = str.charAt(i);
+
+      if (Character.isDigit(letter)) {
+        intermediate = intermediate * 10 + Integer.parseInt(String.valueOf(letter));
+      } else {
+        returnSum += intermediate;
+        intermediate = 0;
+      }
+    }
+
+    if (Character.isDigit(str.charAt(strLength - 1))) {
+      returnSum += intermediate;
+    }
+
+    return returnSum;
+  }
 }
