c28b063a1129ba43823a07995c4b4323e1f820ff
WearyWasTaken <67983460+WearyWasTaken@users.noreply.github.com>
2021-05-08 17:36:58 -0400

    Finished File IO 1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,11 @@
 package gr11review.part2;
 import java.io.*;
+import java.util.Scanner;
 
 public class Utility {
 
     /**
+     * Methods 1
      * Replaces all zips, zaps, and zops with zp
      * @author A. Wong
      */
@@ -15,5 +17,69 @@ public class Utility {
 
     }
 
+    /**
+     * File IO - Read 1
+     * Reades a file and outputs the longest word in the file
+     * @author A. Wong
+     */
+
+     public static String longestWord(String filenametxt) throws IOException{
+
+        File myObj = new File(filenametxt);
+        Scanner myReader = new Scanner(myObj);
+
+        String longestWord = "";
+        int length = 0;
+
+        while (myReader.hasNextLine()){
+
+                String tempString = myReader.nextLine();
+
+                if (tempString.length() >= length){
+
+                    longestWord = tempString;
+                    length = tempString.length();
+
+                }
+
+        }
+
+        myReader.close();
+
+        return longestWord;
+
+     }
+
+     /**
+      * Array 1 One Dimensional
+      *@author A. Wong
+      */
+      
+      public static int[] tenRun(int[] nums){
+
+        int currentMultiple = 0;
+        boolean hasReachedFirst = false;
+
+        for (int counter = 0; counter <= nums.length; counter++){
+
+            if (nums[counter] % 10 == 0){
+
+                currentMultiple = nums[counter];
+                hasReachedFirst = true;
+
+            }
+            else if (hasReachedFirst){
+
+                nums[counter] = currentMultiple;
+
+
+            }
+
+
+        }
+
+        return nums;
+
+      }
     
 }
