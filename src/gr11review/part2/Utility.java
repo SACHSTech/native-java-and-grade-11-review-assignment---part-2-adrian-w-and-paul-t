de9b5b4ddea0c8175ced98a37c3a84c973b3f775
Paul Tran <>
2021-05-09 02:21:32 +0000

    arrays 2 finished file IO problem

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -28,4 +28,39 @@ public class Utility {
 
     return returnSum;
   }
+
+  public static String alphaWord(String filenametxt) {
+    BufferedReader keyboard = new BufferedReader(new FileReader(filenametxt));
+
+    String alphabetical = "";
+    alphabetical = keyboard.readLine();
+    String fileLine = "";
+
+    while (fileLine != "null") {
+      fileLine = keyboard.readLine();
+
+      if (fileLine != "null" && alphabetical.compareTo(fileLine) > 0) {
+        alphabetical = fileLine;
+      }
+    }
+
+    return alphabetical;
+  }
+
+  public static int[] notAlone(int[] nums, int value) {
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == value && i > 0 && i < nums.length - 1) {
+        if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+          if (nums[i + 1] > nums[i - 1]) {
+            nums[i] = nums[i + 1];
+          } else {
+            nums[i] = nums[i - 1];
+          }
+        }
+      }
+    }
+
+    return nums;
+  }
 }
