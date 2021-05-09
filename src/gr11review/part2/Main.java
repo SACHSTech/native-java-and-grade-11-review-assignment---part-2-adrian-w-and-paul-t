9f81f777a3d347439d7b78a74ce94c030d354603
Paul Tran <>
2021-05-09 15:43:56 +0000

    removed line finished array 4

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -4,18 +4,6 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args)throws IOException {
-
-    System.out.println(Utility.sumNumbers("5233 87 hasdfas"));
-
-    System.out.println(Utility.alphaWord("test.txt"));
-
-    int[] array = {1,2,3,4};
-    array = Utility.notAlone(array, 2);
-
-    for (int element: array) {
-      System.out.println(element);
-    }
-
-    System.out.println("hi");
+    
   }
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -32,13 +32,15 @@ public class Utility {
   public static String alphaWord(String filenametxt) {
     BufferedReader keyboard = new BufferedReader(new FileReader(filenametxt));
 
-    String alphabetical = "";
+    String alphabetical;
     alphabetical = keyboard.readLine();
     String fileLine = "";
 
+    //reads file until end
     while (fileLine != "null") {
       fileLine = keyboard.readLine();
 
+      //checks if new word is alphabetically first
       if (fileLine != "null" && alphabetical.compareTo(fileLine) > 0) {
         alphabetical = fileLine;
       }
@@ -49,18 +51,39 @@ public class Utility {
 
   public static int[] notAlone(int[] nums, int value) {
 
+    //Checks if each element of the array with the value is alone
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] == value && i > 0 && i < nums.length - 1) {
         if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
-          if (nums[i + 1] > nums[i - 1]) {
-            nums[i] = nums[i + 1];
-          } else {
-            nums[i] = nums[i - 1];
-          }
+          nums[i] = Math.max(nums[i - 1], nums[i + 1]);
         }
       }
     }
 
     return nums;
   }
+
+  public static boolean canBalance(int[] nums) {
+    int arrayLength = nums.length;
+
+    int arraySum = 0;
+    int leftSide = 0;
+
+    //Calculate sum of array
+    for (int i = 0; i < arrayLength; i++) {
+      arraySum += nums[i];
+    }
+      
+    //If leftSide of array is equal to half of arraySum at any point, array can be split
+    for (int j = 0; j < arrayLength; j++) {
+      leftSide += nums[j];
+            
+      if (leftSide * 2 == arraySum) {
+        return true;
+      }
+    }
+
+    return false;
+	}
+  
 }
